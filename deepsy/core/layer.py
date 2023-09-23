fixed backprop issue and implemented dropout & L2 regularization

deepsy/core/layer.py
@@ -4,22 +4,41 @@ from deepsy.core.functions.activation_functions import ActivationFunction, ReLU
 
 
 class Layer:
-    def __init__(self, nr_neurons, nr_in_features, activation_func: ActivationFunction):
+    def __init__(self, nr_neurons, nr_in_features, activation_func: ActivationFunction, dropout_rate=0.0):
         self._nr_neurons = nr_neurons
         self._nr_in_features = nr_in_features
         self._activation_func = activation_func
         self._init_parameters_and_gradients()
+        self._dropout_rate = dropout_rate
 
-    def forward_prop(self, X):
+    def forward_prop(self, X, is_train):
         assert self._params['W'].shape[1] == X.shape[0], 'Shape of input = {} does not correspond to the shape of the weights = {}'.format(X.shape, self._params['W'].shape)
         self.X = X
         self.Z = np.dot(self._params['W'], self.X) + self._params['b']
-        return self._activation_func.activate(self.Z)
+        A = self._activation_func.activate(self.Z)
+
+        if is_train and self._dropout_rate > 0:
+            D = np.random.rand(A.shape[0], A.shape[1])
+            D = (D > self._dropout_rate).astype(int) 
+            A *= D
+            A /= (1 - self._dropout_rate)
+
+            self.D = D
+
+        return A
     
-    def backward_prop(self, dA):
+    def backward_prop(self, dA, reg_lambda):
+        if self._dropout_rate > 0:
+            dA *= self.D
+            dA /= (1 - self._dropout_rate) 
+
         dZ = dA * self._activation_func.derivate(self.Z)
         self._grads['dW'] = np.dot(dZ, self.X.T) / dZ.shape[1]
         self._grads['db'] = np.sum(dZ, axis=1, keepdims=True) / dZ.shape[1]
+
+        if reg_lambda > 0:
+            self._grads['dW'] += reg_lambda / self.X.shape[1] * self._params['W']
+
         return np.dot(self._params['W'].T, dZ)
     
     def summary(self):

deepsy/core/model.py
@@ -10,40 +10,31 @@ class Model:
         self._loss_func = loss_func
         self.optimizer = optimizer
 
-    def train(self, X, Y, nr_epochs, batch_size=64):
+    def train(self, X, Y, nr_epochs, reg_lambda=0.0):
         for epoch in range(nr_epochs):
-            batch_start = 0
-            cost = 0
-            nr_batches = float(len(X) / batch_size)
-            while (batch_start < len(X)):
-                batch_end = min(batch_start + batch_size, len(X[0]))
-                X_batch = X[:, batch_start : batch_end]
-                Y_batch = Y[batch_start : batch_end]
+            # forward propagation
+            Y_predicted = self._neural_network.forward_prop(input=X, is_train=True)
 
-                # forward propagation
-                Y_batch_predicted = self._neural_network.forward_prop(input=X_batch)
+            # compute cost
+            cost = np.mean(self._loss_func.get_loss(Y_predicted, Y))
+            if reg_lambda > 0:
+                cost += self._calc_reg_cost_term(reg_lambda, X.shape[1])
 
-                # compute loss & cost
-                cost += np.mean(self._loss_func.get_loss(Y_batch_predicted, Y_batch))
+            # backward propagation: compute gradients for each layer
+            self._neural_network.backward_prop(Y_predicted - Y, reg_lambda)
 
-                # backward propagation: compute gradients for each layer
-                self._neural_network.backward_prop(self._loss_func.derivate())
+            # update gradients
+            self.optimizer.step(nn_parameters=self._neural_network.get_parameters(), nn_gradients=self._neural_network.get_gradients())
 
-                # update gradients
-                self.optimizer.step(nn_parameters=self._neural_network.get_parameters(), nn_gradients=self._neural_network.get_gradients())
-
-                batch_start += batch_size
-
-            cost = cost / nr_batches
             print('Epoch {}: cost = {}'.format(epoch + 1, cost))
 
     def validate(self, X, Y):
-        Y_predicted = self._neural_network.forward_prop(input=X)
+        Y_predicted = self._neural_network.forward_prop(input=X, is_train=False)
         cost = np.mean(self._loss_func.get_loss(Y_predicted, Y))
         return Y_predicted, cost
 
     def predict(self, X):
-        return self._neural_network.forward_prop(X)
+        return self._neural_network.forward_prop(X, is_train=False)
     
     def summary(self):
         print('Model summary:')
@@ -52,3 +43,10 @@ class Model:
     
     def get_neural_network(self):
         return self._neural_network
+    
+    def _calc_reg_cost_term(self, reg_lambda, m):
+        reg_term = 0.0
+        for layer_params in self._neural_network.get_parameters():
+            reg_term += np.sum(np.square(layer_params['W']))
+        reg_term *= 0.5 * reg_lambda / m
+        return reg_term